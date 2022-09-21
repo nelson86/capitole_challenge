719c2355661d69dbf76a7b40568511deb9a9a6d2
Nelson Arabel <nelson.arabel@gmail.com>
2022-09-21 12:52:21 -0300

    Building hexagonal architecture in project

---
+++ web/src/main/java/com/capitole_consulting/WebApplication.java
@@ -0,0 +1,13 @@
+package com.capitole_consulting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+
+}
